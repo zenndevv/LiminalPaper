fe73ed53594da0a88eb7552caf171e2120ff5430
zenntet <airkraftvideos@gmail.com>
2026-05-26 19:03:09 -0500

    stack overflow suppression options

---
+++ paper-server/src/main/java/net/zenntet/liminal/ConfigConstants.java
@@ -0,0 +1,19 @@
+package net.zenntet.liminal;
+
+public class ConfigConstants {
+
+    // All from AntiShadowPatch
+    public static final boolean BringBackStackOverflowSuppression = true;
+    public static final boolean BringBackClassCastExceptionSuppression = true;
+    public static final boolean BringBackTrapdoorUpdateSkipping = true;
+    public static final boolean BringBackFloatingRedstoneComponents = true;
+    public static final boolean BringBackFurnaceXPDupe = true;
+    public static final boolean BringBackTileEntitySwap = true;
+    public static final boolean KeepSwappedBlockEntities = true;
+    public static final boolean BringBack_1_17_ItemShadowing = true;
+    public static final boolean BringBack_1_18_ItemShadowing = true;
+    public static final boolean BringBackGracefulStackOverflowHandling = true;
+    public static final boolean BringBackGracefulOutOfMemoryHandling = true;
+
+    // All from NotSoShadowExtras
+}
